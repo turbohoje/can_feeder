61dac505347b3aca1c52aaa5d6f013e08212115d
justin meyer <jumeyer@adobe.com>
2021-07-01 16:29:48 -0600

    arg based feeder and seconds

--- code/open.py
+++ code/open.py
@@ -11,11 +11,34 @@ import time
 
 #for p in [36, 38, 40] :
 
-p=40
+feeder = None
+seconds = None
+
+def err(str):
+    print(str)
+    exit(2)
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"f:s:",["feeder=","seconds="])
+except getopt.GetoptError:
+    err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+for opt, arg in opts:
+    if opt in ['-f', '--feeder', '--feeder=']:
+        try:
+            feeder = [0,36,38,40][int(arg)]
+        except:
+            err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+    elif opt in ['-s', '--seconds', '--seconds=']:
+        seconds = int(arg)
+if(feeder == None or seconds == None):
+    err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+
+p=feeder
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(p,GPIO.OUT)
 GPIO.output(p,GPIO.LOW)
-time.sleep(52)
+time.sleep(seconds)
 GPIO.output(p,GPIO.HIGH)
 
 GPIO.cleanup()
--- code/reverse.py
+++ code/reverse.py
@@ -2,6 +2,8 @@
 
 import RPi.GPIO as GPIO
 import time
+import sys, getopt
+
 
 # GPIOid - rpi-PIN
 # 16 - 36
@@ -12,10 +14,32 @@ import time
 # 26 - 37
 
 #for p in [36, 38, 40] :
-
 rev_pins = [35, 37]
 
-p=38
+feeder = None
+seconds = None
+
+def err(str):
+    print(str)
+    exit(2)
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"f:s:",["feeder=","seconds="])
+except getopt.GetoptError:
+    err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+for opt, arg in opts:
+    if opt in ['-f', '--feeder', '--feeder=']:
+        try:
+            feeder = [0,36,38,40][int(arg)]
+        except:
+            err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+    elif opt in ['-s', '--seconds', '--seconds=']:
+        seconds = int(arg)
+if(feeder == None or seconds == None):
+    err(__file__ + ' -f <feeder> -s <seconds> | --feeder=<feeder> --seconds=<seconds>')
+
+p=feeder
 
 
 #GPIO.setwarnings(False)
@@ -30,7 +54,7 @@ for r in rev_pins:
 
 GPIO.output(p,GPIO.LOW)
 
-time.sleep( 38)
+time.sleep(seconds)
 
 for r in rev_pins:
     GPIO.output(r,GPIO.HIGH)
